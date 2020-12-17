115ee4321eb0220867a40e1d36d9b1aeec5beef4
joe walsh <joewalsh@vistulaofindiana.com>
2020-12-17 14:24:48 -0500

    pagination beta work

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 import csv
 import logging
 import sys
+from math import ceil
 from dataclasses import dataclass
 from datetime import date, datetime, time, timedelta
 from decimal import ROUND_05UP, ROUND_HALF_UP, Decimal
@@ -13,7 +14,7 @@ from app import login
 from sqlalchemy import (Column, Date, ForeignKey, Integer, Numeric, String,
                         between, create_engine, extract, or_, and_)
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, sessionmaker, with_polymorphic
+from sqlalchemy.orm import Query, relationship, sessionmaker, with_polymorphic
 from utils import utils
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask_login import UserMixin
@@ -27,14 +28,71 @@ logging.basicConfig(
 
 Base = declarative_base() # We need to inherit Base in order to register models with SQA.
 engine = create_engine(Config.DATABASE_URI_PANDAS, pool_size=20)
+
+class MyQuery(Query):
+
+    def paginate(self, page=None, per_page=None, error_out=True, max_per_page=None):
+
+        request=False
+    
+        if request:
+            if page is None:
+                try:
+                    page = int(request.args.get('page', 1))
+                except (TypeError, ValueError):
+                    if error_out:
+                        abort(404)
+
+                    page = 1
+
+            if per_page is None:
+                try:
+                    per_page = int(request.args.get('per_page', 20))
+                except (TypeError, ValueError):
+                    if error_out:
+                        abort(404)
+
+                    per_page = 20
+        else:
+            if page is None:
+                page = 1
+
+            if per_page is None:
+                per_page = 20
+
+        if max_per_page is not None:
+            per_page = min(per_page, max_per_page)
+
+        if page < 1:
+            if error_out:
+                abort(404)
+            else:
+                page = 1
+
+        if per_page < 0:
+            if error_out:
+                abort(404)
+            else:
+                per_page = 20
+
+        items = self.limit(per_page).offset((page - 1) * per_page).all()
+
+        if not items and page != 1 and error_out:
+            abort(404)
+
+        total = self.order_by(None).count()
+
+        return Pagination(self, page, per_page, total, items)
+
 Session = sessionmaker(bind=engine, expire_on_commit=False)
 
 #TODO: try to keep session alive across function calls instead of instantiating it in function
+#  I CAN use context manager for session
+
 @login.user_loader
 def load_user(id):
     s = Session()
     return s.query(User).get(int(id))
-
 
 class User(UserMixin, Base):
     __tablename__ = 'user'
@@ -1737,7 +1795,107 @@ class Export(object):
 
                 cw.writerow(transfer_gen)
 
+class Pagination(object):
+    """Internal helper class returned by :meth:`Query.paginate`.  You
+    can also construct it from any other SQLAlchemy query object if you are
+    working with other libraries.  Additionally it is possible to pass `None`
+    as query object in which case the :meth:`prev` and :meth:`next` will
+    no longer work.
+    """
 
+    def __init__(self, query, page, per_page, total, items):
+        #: the unlimited query object that was used to create this
+        #: pagination object.
+        self.query = query
+        #: the current page number (1 indexed)
+        self.page = page
+        #: the number of items to be displayed on a page.
+        self.per_page = per_page
+        #: the total number of items matching the query
+        self.total = total
+        #: the items for the current page
+        self.items = items
+
+    @property
+    def pages(self):
+        """The total number of pages"""
+        if self.per_page == 0:
+            pages = 0
+        else:
+            pages = int(ceil(self.total / float(self.per_page)))
+        return pages
+
+    def prev(self, error_out=False):
+        """Returns a :class:`Pagination` object for the previous page."""
+        assert self.query is not None, 'a query object is required ' \
+                                       'for this method to work'
+        return self.query.paginate(self.page - 1, self.per_page, error_out)
+
+    @property
+    def prev_num(self):
+        """Number of the previous page."""
+        if not self.has_prev:
+            return None
+        return self.page - 1
+
+    @property
+    def has_prev(self):
+        """True if a previous page exists"""
+        return self.page > 1
+
+    def next(self, error_out=False):
+        """Returns a :class:`Pagination` object for the next page."""
+        assert self.query is not None, 'a query object is required ' \
+                                       'for this method to work'
+        return self.query.paginate(self.page + 1, self.per_page, error_out)
+
+    @property
+    def has_next(self):
+        """True if a next page exists."""
+        return self.page < self.pages
+
+    @property
+    def next_num(self):
+        """Number of the next page"""
+        if not self.has_next:
+            return None
+        return self.page + 1
+
+    def iter_pages(self, left_edge=2, left_current=2,
+                   right_current=5, right_edge=2):
+        """Iterates over the page numbers in the pagination.  The four
+        parameters control the thresholds how many numbers should be produced
+        from the sides.  Skipped page numbers are represented as `None`.
+        This is how you could render such a pagination in the templates:
+
+        .. sourcecode:: html+jinja
+
+            {% macro render_pagination(pagination, endpoint) %}
+              <div class=pagination>
+              {%- for page in pagination.iter_pages() %}
+                {% if page %}
+                  {% if page != pagination.page %}
+                    <a href="{{ url_for(endpoint, page=page) }}">{{ page }}</a>
+                  {% else %}
+                    <strong>{{ page }}</strong>
+                  {% endif %}
+                {% else %}
+                  <span class=ellipsis>…</span>
+                {% endif %}
+              {%- endfor %}
+              </div>
+            {% endmacro %}
+        """
+        last = 0
+        for num in range(1, self.pages + 1):
+            if num <= left_edge or \
+               (num > self.page - left_current - 1 and
+                num < self.page + right_current) or \
+               num > self.pages - right_edge:
+                if last + 1 != num:
+                    yield None
+                yield num
+                last = num
 
 
 
